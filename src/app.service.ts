3253e9072c0a787b290f4584392c461623081e43
poamen <poamen@appzonegroup.com>
2023-09-01 04:24:11 +0100

    added statics

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getHello2(): any {
+    return { message: 'Hello World!', title: 'My Greeting App' };
+  }
+  getHome(): any {
+    return { title: 'Home Page' };
+  }
+  getAboutUs(): any {
+    return { title: 'About Us Page' };
+  }
 }
